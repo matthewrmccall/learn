69f9a0b53b27a7711df6af0771807291c4ae29d9
matthewrmccall <matt@mrmccall.com>
2021-02-21 16:47:14 -0500

    Added more to state

--- State.js
+++ State.js
@@ -3,7 +3,17 @@
 
 // Converting a function to a class:
 // 1. Create an ES6 class, with the same name, that extends React.Component
+class Clock extends React.Component() {
 // 2. Add a single empty method to it called render()
+render() {
 // 3. Move the body of the function into the render method
-// 4. Replace props with this.props in the render() body
+  return (
+      <div>
+        <h1>Hello, world!</h1>
+         // 4. Replace props with this.props in the render() body
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
 // 5. Delete the remaining empty function declaration
--- Subjects/ClockComponentPreState.js
+++ Subjects/ClockComponentPreState.js
@@ -23,7 +23,7 @@ function Clock(props) {
     <div>
       <h1>Hello World!</h1>
       <h2>It is {props.date.toLocaleTimeString()}</h2>
-    <div>
+    </div>
   );
 }
 
